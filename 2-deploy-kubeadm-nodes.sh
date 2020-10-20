a77c0c71b8f65fad49f585f46d271f411cb9f829
packetcraft <rakeshsingh005@gmail.com>
2020-10-20 10:53:12 +0530

    Update 2-deploy-kubeadm-nodes.sh

--- 2-deploy-kubeadm-nodes.sh
+++ 2-deploy-kubeadm-nodes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NODES=$(echo worker{1..2})
+NODES=$(echo worker6{1..2})
 for NODE in ${NODES}; do multipass launch --name ${NODE} --cpus 2 --mem 2G --disk 8G 18.04; done
 
 for NODE in ${NODES}; do
@@ -7,17 +7,20 @@ multipass exec ${NODE} -- bash -c 'wget https://packages.cloud.google.com/apt/do
 multipass exec ${NODE} -- bash -c 'sudo apt-key add apt-key.gpg'
 multipass exec ${NODE} -- bash -c 'sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"'
 multipass exec ${NODE} -- bash -c 'sudo apt-get update && apt-get install -y apt-transport-https'
-multipass exec ${NODE} -- bash -c 'curl https://releases.rancher.com/install-docker/18.09.sh | sh'
+# multipass exec ${NODE} -- bash -c 'curl https://releases.rancher.com/install-docker/18.09.sh | sh'
+multipass exec ${NODE} -- bash -c 'sudo curl -fsSL https://get.docker.com -o get-docker.sh'
+multipass exec ${NODE} -- bash -c 'sudo sh get-docker.sh'
 # Setup daemon.
-multipass transfer daemon.json ${NODE}:
-multipass exec ${NODE} -- bash -c 'sudo cp /home/ubuntu/daemon.json /etc/docker/daemon.json'
-multipass exec ${NODE} -- bash -c 'sudo mkdir -p /etc/systemd/system/docker.service.d'
+# multipass transfer daemon.json ${NODE}:
+# multipass exec ${NODE} -- bash -c 'sudo cp /home/ubuntu/daemon.json /etc/docker/daemon.json'
+# multipass exec ${NODE} -- bash -c 'sudo mkdir -p /etc/systemd/system/docker.service.d'
 # Restart docker.
 multipass exec ${NODE} -- bash -c 'sudo systemctl daemon-reload'
 multipass exec ${NODE} -- bash -c 'sudo systemctl restart docker'
 multipass exec ${NODE} -- bash -c 'sudo usermod -aG docker ubuntu'
 multipass exec ${NODE} -- bash -c 'sudo apt-get install -y kubelet kubeadm kubectl'
-multipass exec ${NODE} -- bash -c 'sudo apt-mark hold kubelet kubeadm kubectl'
+# multipass exec ${NODE} -- bash -c 'sudo apt-mark hold kubelet kubeadm kubectl'
+multipass exec master5 -- bash -c 'sudo apt-get install -y kubelet=1.19.3-00 kubeadm=1.19.3-00 kubectl=1.19.3-00'
 multipass exec ${NODE} -- bash -c 'sudo swapoff -a'
 multipass exec ${NODE} -- bash -c  "sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab"
 multipass exec ${NODE} -- bash -c 'sudo sysctl net.bridge.bridge-nf-call-iptables=1'
